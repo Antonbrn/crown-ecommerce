64afd24b05b5788d3852228090ddac5ee9d0939f
Antonbrn <anton.brannstrom@cmeducations.com>
2021-05-08 20:21:57 +0200

    implementerat firebase utils, funktionalitet för att spara authentikerade användare till firestore db

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import "./sign-in-and-sign-up.styles.scss";
 
 import Signin from "../../components/sign-in/Sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <Signin />
+    <SignUp />
   </div>
 );
 
